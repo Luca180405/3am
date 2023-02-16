d473573bc6485f77ca440481904d59ecd535c5da
Luca180405 <daguannoluca16@gmail.com>
2023-02-16 12:20:25 +0100

    .

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -5,12 +5,14 @@
     metatags.description = 'Description coming soon...'
 </script>
 
-<h1>3AM</h1>
-
-<p>
-    To see an example app that shows off a lot of Routify's features, go to <a href="/example">/example</a>
-</p>
-
-<p>
-    This template is ready to be used in production! just delete the example app at: src/pages/example
-</p>
+<bo class="min-h-screen">
+    <header style="padding: 30px 100px;" class="absolute top-0 left-0 flex items-center justify-between w-full text-white align-center">
+        <a href="#" class="logo">Logo</a>
+        <ul class="flex items-center justify-center">
+            <li class="ml-20"><a href="#" style="padding: 6px 15px;" class="rounded-2xl"></a>Test</li>
+            <li class="ml-20"><a href="#" style="padding: 6px 15px;" class="rounded-2xl"></a>Test</li>
+            <li class="ml-20"><a href="#" style="padding: 6px 15px;" class="rounded-2xl"></a>Test</li>
+            <li class="ml-20"><a href="#" style="padding: 6px 15px;" class="rounded-2xl"></a>Test</li>
+        </ul>
+    </header>
+</bo>
