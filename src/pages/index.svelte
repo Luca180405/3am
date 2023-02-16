df24a7775cd0ec4692aae484efe45873bcfbe367
Luca180405 <daguannoluca16@gmail.com>
2023-02-16 12:45:56 +0100

    add logo

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <bo class="min-h-screen">
-    <header style="padding: 30px 100px;" class="absolute top-0 left-0 flex items-center justify-between w-full text-white align-center">
-        <a href="#" class="logo">Logo</a>
+    <header style="padding: 0px 100px;" class="absolute top-0 left-0 flex items-center justify-between w-full text-white align-center">
+        <a href="#" class="logo"><img src="\public\TRANSPARENT LOGO.png" alt="" height="256" width="256"></a>
         <ul class="flex items-center justify-center">
             <li class="ml-20"><a href="#" style="padding: 6px 15px;" class="rounded-2xl"></a>Test</li>
             <li class="ml-20"><a href="#" style="padding: 6px 15px;" class="rounded-2xl"></a>Test</li>
